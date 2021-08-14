df3ddd7ec3e94eeffbe2f69fdd44ed77f5ac0c1b
yeezick <ramerick5@gmail.com>
2021-08-13 21:03:00 -0400

    leaving off in settings.jsx, i believe i managed to get user state to persist so that the api_id can be successfully destructured and use updateApi

--- client/src/screens/settings/Settings.jsx
+++ client/src/screens/settings/Settings.jsx
@@ -1,11 +1,53 @@
-
+import {useState, useEffect} from "react"
+import {useHistory} from "react-router-dom"
+import { updateApi } from "../../services/api"
 import "./Settings.css"
 
-const Settings = () => {
+const Settings = ({user}) => {
+
+  const history = useHistory();
+  const [apiList, setApiList] = useState({
+    cat: false,
+    crypto: false,
+    fact: false,
+    food: false,
+    joke: false,
+    news: false,
+    quote: false,
+    weather: false
+  })
+  const {id} = user.apis[0]
+
+  const handleChange = (e) => {
+    const {name, value} = e.target
+    setApiList({
+      [name]: value
+    })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault() 
+    await updateApi(id, apiList)
+    history.push(`/welcome`)
+  }
+  // console.log("before:", user.apis)
+
+  useEffect(() => {
+    setApiList(user.apis[0])
+  },[])
+
   return (
-    <div>
-      Setting Screen
-    </div>
+        <div>
+
+      <h1>Settings screen</h1>
+      <form onSubmit={(e) => handleSubmit(e)}>
+
+
+        <button type="submit">
+          Submit
+        </button>
+      </form>
+        </div>
   )
 }
 
