4331a91da6c80c8438a3e975931b6f0a495e1d07
yeezick <ramerick5@gmail.com>
2021-08-16 21:11:01 -0400

    login screen near completion

--- client/src/screens/login/Login.jsx
+++ client/src/screens/login/Login.jsx
@@ -1,6 +1,6 @@
 import "./Login.css";
 
-const Login = ({setLoginForm, loginForm, handleLogin}) => {
+const Login = ({ setLoginForm, loginForm, handleLogin }) => {
   const handleChange = (e) => {
     const { name, value } = e.target;
     setLoginForm((prevForm) => ({
@@ -10,24 +10,42 @@ const Login = ({setLoginForm, loginForm, handleLogin}) => {
   };
 
   return (
-    <div>
-      <h1>Log in screen</h1>
-      <form onSubmit={(e) => handleLogin(e)}>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="text"
-          name="email"
-          value={loginForm.email}
-          onChange={(e) => handleChange(e)}
-        />
+    <div className="login">
+      <h2 className="login-title">Your home is yours only.</h2>
+      <form className="login-form" onSubmit={(e) => handleLogin(e)}>
+        <p className="login-subtitle">
+          If you lost your key, we can get it right back to you, just log in!{" "}
+        </p>
 
-        <label htmlFor="password">Password:</label>
-        <input 
-        type="password" 
-        name="password" 
-        value={loginForm.password}
-        onChange={(e) => handleChange(e)} />
-        <button type="submit">Submit</button>
+        <div className="login-item">
+          <label className="login-label" htmlFor="email">
+            Email:
+          </label>
+          <input
+            className="login-input"
+            type="text"
+            name="email"
+            value={loginForm.email}
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+
+        <div className="login-item">
+          <label className="login-label" htmlFor="password">
+            Password:
+          </label>
+          <input
+            className="login-input"
+            type="password"
+            name="password"
+            value={loginForm.password}
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+
+        <button className="login-button" type="submit">
+          Generate key
+        </button>
       </form>
     </div>
   );
