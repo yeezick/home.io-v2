7a16f31fa1e0d3435ebc956962fbfcb5a5a5065f
yeezick <ramerick5@gmail.com>
2021-08-18 10:28:18 -0400

    adding icons and increasing font size within header and footer

--- client/src/layout/footer/Footer.jsx
+++ client/src/layout/footer/Footer.jsx
@@ -1,3 +1,5 @@
+import {GoMarkGithub} from "react-icons/go"
+import {SiLinkedin, SiMailDotRu, SiTwitter} from "react-icons/si"
 import "./Footer.css";
 
 const Footer = () => {
@@ -5,8 +7,8 @@ const Footer = () => {
     <footer>
       <div className="about">
         <hr/>
-        <h4 className="footer-title"> From the Creator:</h4>
-        <p className="about-text">
+        <h4 className="footer-title footer-text"> From the Creator:</h4>
+        <p className="footer-text">
           I hope you enjoyed the site! During this project, I was a student of
           General Assembly's Software Engineering Immersive. Below are my
           networks, but if you'd like to see the codebase, OctoCat might know
@@ -14,36 +16,36 @@ const Footer = () => {
         </p>
       </div>
       <div className="networks">
-        <h4 className="footer-title">Let's Connect!</h4>
+        <h4 className="footer-title footer-text">Let's Connect!</h4>
         <div className="network-link-container">
 
         <a href="https://github.com/yeezick" className="network-link">
-          GH
+          <GoMarkGithub size={50}/>
         </a>
         <a
           href="https://www.linkedin.com/in/erick-manrique/"
           className="network-link"
           >
-          LI
+          <SiLinkedin size={50}/>
         </a>
         <a href="https://twitter.com/erckmun" className="network-link">
-          TW
+          <SiTwitter size={50}/>
         </a>
         <a
           href="mailto:ramerick5@gmail.com?subject=Nice%20Webpage"
           className="network-link"
           >
-          EM
+          <SiMailDotRu size={50}/>
         </a>
           </div>
       </div>
       <div className="support">
-        <h4 className="footer-title">Special Thanks:</h4>
-        <a className="support-link" href="https://generalassemb.ly/">
+        <h4 className="footer-title footer-text">Special Thanks:</h4>
+        <a className="support-link footer-text" href="https://generalassemb.ly/">
           General Assembly
         </a>
         <a
-          className="support-link"
+          className="support-link footer-text"
           href="https://www.udemy.com/user/4b4368a3-b5c8-4529-aa65-2056ec31f37e/"
         >
           Angela Yu
