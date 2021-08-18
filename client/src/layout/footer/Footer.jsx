0595521161ea11201a180f59f7b6dd2d9dcd0e73
yeezick <ramerick5@gmail.com>
2021-08-18 11:16:10 -0400

    adding url link to readme and octocat link to repo in footer

--- client/src/layout/footer/Footer.jsx
+++ client/src/layout/footer/Footer.jsx
@@ -14,26 +14,30 @@ const Footer = () => {
           networks, but if you'd like to see the codebase, OctoCat might know
           the way...
         </p>
+        <a href="https://github.com/yeezick/home.io-v2"
+          className="network-link octocat-link" rel="noreferrer">
+        <img className="octocat" src="/octocat-jedi.png" alt="octo cat jedi"/>
+          </a>
       </div>
       <div className="networks">
         <h4 className="footer-title footer-text">Let's Connect!</h4>
         <div className="network-link-container">
 
-        <a href="https://github.com/yeezick" className="network-link">
+        <a href="https://github.com/yeezick" className="network-link" rel="noreferrer">
           <GoMarkGithub size={50}/>
         </a>
         <a
           href="https://www.linkedin.com/in/erick-manrique/"
-          className="network-link"
+          className="network-link" rel="noreferrer"
           >
           <SiLinkedin size={50}/>
         </a>
-        <a href="https://twitter.com/erckmun" className="network-link">
+        <a href="https://twitter.com/erckmun" className="network-link" rel="noreferrer">
           <SiTwitter size={50}/>
         </a>
         <a
           href="mailto:ramerick5@gmail.com?subject=Nice%20Webpage"
-          className="network-link"
+          className="network-link" rel="noreferrer"
           >
           <SiMailDotRu size={50}/>
         </a>
@@ -41,17 +45,17 @@ const Footer = () => {
       </div>
       <div className="support">
         <h4 className="footer-title footer-text">Special Thanks:</h4>
-        <a className="support-link footer-text" href="https://generalassemb.ly/">
+        <a className="support-link footer-text" href="https://generalassemb.ly/" rel="noreferrer">
           General Assembly
         </a>
         <a
           className="support-link footer-text"
-          href="https://www.udemy.com/user/4b4368a3-b5c8-4529-aa65-2056ec31f37e/"
+          href="https://www.udemy.com/user/4b4368a3-b5c8-4529-aa65-2056ec31f37e/"  rel="noreferrer"
         >
           Angela Yu
         </a>
       </div>
-      <p>Disclaimer: if you are an employer on desktop, please note this full-stack site was built in a 7 day timeframe with a mobile-first approach. Revisiting this site to optimize desk-top view (as well as animating the site) lives among my list of todos. </p>
+      <p><small>Disclaimer: if you are an employer on desktop, please note this full-stack site was built in a 7 day timeframe with a mobile-first approach. Revisiting this site to optimize desk-top view (as well as animating the site) lives among my list of todos.</small> </p>
     </footer>
   );
 };
